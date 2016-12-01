dc4fd39794b8f926c63348ba351e24c344305428
Aleix Pol <aleixpol@kde.org>
2016-12-01 17:49:11 +0100

    Initial

---
+++ solomagic.py
@@ -0,0 +1,72 @@
+import argparse
+
+class Block:
+    entries = []
+
+    def __init__(self, entries = []):
+        self.entries = entries
+
+    def getEntry(self, start):
+        for v in self.entries:
+            if v[0] == start:
+                return v
+        raise "Scheisse"
+
+    def doPrint(self):
+        for entry in self.entries:
+            print(" ".join(entry))
+
+class FileInstance:
+    blocks = []
+
+    def addBlock(self, block):
+        self.blocks.append(block)
+
+    def doPrint(self):
+        for block in self.blocks:
+            block.doPrint()
+
+def parse(f):
+    currentBlock = Block()
+    ret = FileInstance()
+
+    for line in f:
+        line = line.rstrip()
+        if len(line) == 0:
+            ret.addBlock(currentBlock)
+            currentBlock = Block()
+        else:
+            currentBlock.entries.append(line.split())
+
+    ret.addBlock(currentBlock)
+    return ret
+
+def specialReplace(word, a, b):
+    if "question" in word:
+        return word
+    else:
+        return word.replace(a, b)
+
+def createMa(block):
+    mtEntry = block.getEntry("\mt")
+    maEntry = ["\\ma"] + [word for word in mtEntry[1:] if word != '/']
+    block.entries.append(maEntry)
+    return block
+
+blockOperations = {
+    "QtoApostrophe": lambda block: Block([ [specialReplace(word, "q", "'") for word in entry ] for entry in block.entries]),
+    "createMa": createMa
+}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input text file")
+    parser.add_argument('rules', metavar='N', type=str, nargs='*', help='one of these: ' +", ".join(blockOperations.keys()))
+    args = parser.parse_args()
+
+    with open(args.input, 'r') as f:
+        instance = parse(f)
+        for rule in args.rules:
+            cosa = [blockOperations[rule](block) for block in instance.blocks]
+            instance.blocks = cosa
+        instance.doPrint()
---
+++ tests/test.py
@@ -0,0 +1,24 @@
+import unittest
+import subprocess
+
+def callTest(fileNameInput, fileNameOutput = None, extraarg = []):
+    if fileNameOutput == None:
+        fileNameOutput = fileNameInput
+    with open('testfile', 'wb') as f:
+        subprocess.check_call(["python3", "solomagic.py", fileNameInput] + extraarg, stdout=f)
+
+    # I think whitespace changes don't matter
+    return subprocess.check_call(["diff", "-ub", "testfile", fileNameOutput])
+
+class TestIO(unittest.TestCase):
+
+    def test_invariant(self):
+        self.assertEqual(callTest("tests/a.text.in"), 0)
+        self.assertEqual(callTest("tests/b.text.in"), 0)
+
+    def test_process(self):
+        self.assertEqual(callTest("tests/a.text.in", "tests/a.text.out", ["QtoApostrophe"]), 0)
+        self.assertEqual(callTest("tests/b.text.in", "tests/b.text.out", ["createMa", "QtoApostrophe"]), 0)
+
+if __name__ == '__main__':
+    unittest.main()
