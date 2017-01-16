d4466fc26e3b55d72384ec3e92fdae755d6e515d
Aleix Pol <aleixpol@kde.org>
2017-01-16 04:18:48 +0100

    Rename blocks into records
    It will make it easier to read altogether

--- solomagic.py
+++ solomagic.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-class Block:
+class Record:
     def __init__(self, t = []):
         self.tiers = t
 
@@ -25,38 +25,38 @@ class Block:
             print(" ".join(tier))
 
     def __repr__(self):
-        return "Block(%s tiers)" % len(self.tiers)
+        return "Record(%s tiers)" % len(self.tiers)
 
 class FileInstance:
-    blocks = []
+    records = []
 
-    def addBlock(self, block):
-        self.blocks.append(block)
+    def addRecord(self, record):
+        self.records.append(record)
 
     def doPrint(self):
         first = True
-        for block in self.blocks:
+        for record in self.records:
             if not first:
                 print()
-            block.doPrint()
+            record.doPrint()
             first = False
 
 def parse(f):
-    currentBlock = Block()
+    currentRecord = Record()
     ret = FileInstance()
 
     for line in f:
         line = line.rstrip()
         if not line:
             continue
-        elif line.startswith("\\ref") and currentBlock.tiers:
-            ret.addBlock(currentBlock)
-            currentBlock = Block([])
+        elif line.startswith("\\ref") and currentRecord.tiers:
+            ret.addRecord(currentRecord)
+            currentRecord = Record([])
 
-        currentBlock.tiers.append(line.split())
+        currentRecord.tiers.append(line.split())
 
-    if currentBlock.tiers:
-        ret.addBlock(currentBlock)
+    if currentRecord.tiers:
+        ret.addRecord(currentRecord)
     return ret
 
 def specialReplace(word, a, b):
@@ -65,30 +65,30 @@ def specialReplace(word, a, b):
     else:
         return word.replace(a, b)
 
-def createMa(block, function = None):
-    mtTier = block.getTier("\\mt")
+def createMa(record, function = None):
+    mtTier = record.getTier("\\mt")
     maTier = ["\\ma"] + [word for word in mtTier[1:] if word != '/']
     if function:
         maTier = function(maTier)
-    block.tiers.append(maTier)
-    return block
+    record.tiers.append(maTier)
+    return record
 
-blockOperations = {
-    "QToApostrophe": lambda block: Block([ [specialReplace(word, "q", "'") for word in tier ] for tier in block.tiers]),
-    "ApostropheToQ": lambda block: Block([ [word.replace("'", "q") for word in tier ] for tier in block.tiers]),
-    "IvenaToIvaEna": lambda block: createMa(block, lambda tier: (word.replace("ivena", "iva ena") for word in tier)),
+recordOperations = {
+    "QToApostrophe": lambda record: Record([ [specialReplace(word, "q", "'") for word in tier ] for tier in record.tiers]),
+    "ApostropheToQ": lambda record: Record([ [word.replace("'", "q") for word in tier ] for tier in record.tiers]),
+    "IvenaToIvaEna": lambda record: createMa(record, lambda tier: (word.replace("ivena", "iva ena") for word in tier)),
     "createMa": createMa
 }
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input text file")
-    parser.add_argument('rules', metavar='rule', type=str, nargs='*', help='one of these: ' +", ".join(blockOperations.keys()))
+    parser.add_argument('rules', metavar='rule', type=str, nargs='*', help='one of these: ' +", ".join(recordOperations.keys()))
     args = parser.parse_args()
 
     with open(args.input, 'r') as f:
         instance = parse(f)
         for rule in args.rules:
-            cosa = [blockOperations[rule](block) for block in instance.blocks]
-            instance.blocks = cosa
+            cosa = [recordOperations[rule](record) for record in instance.records]
+            instance.records = cosa
         instance.doPrint()
