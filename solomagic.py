b3ab0ab7186b57030f18e64d54004f804022f074
Aleix Pol <aleixpol@kde.org>
2017-01-20 03:03:29 +0100

    Integrate rules, make sure they are work
    Also integrate the tests

--- solomagic.py
+++ solomagic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
 
 class Record:
     tiers = []
@@ -41,6 +42,10 @@ class Record:
         blanklines = self.emptyLines.get(i, 0)
         for j in range(0, blanklines):
             print()
+
+    def clean(self):
+        self.tiers = map(lambda tier: filter(lambda word: word, tier), self.tiers)
+        return self
 
     def __repr__(self):
         return "Record(%s tiers)" % len(self.tiers)
@@ -91,20 +96,62 @@ def specialReplace(word, a, b):
         return word.replace(a, b)
 
 def createMa(record, function = None):
-    mtTier = record.getTier("\\mt")
-    if not mtTier:
-        return record
-    maTier = ["\\ma"] + [word for word in mtTier[1:] if word != '/']
+    maTier = record.getTier("\\ma")
+    if not maTier or len(maTier)<=1:
+        mtTier = record.getTier("\\mt")
+        if not mtTier:
+            return record
+        maTier = ["\\ma"] + [word for word in mtTier[1:] if word != '/']
+
     if function:
         maTier = function(maTier)
     record.setTier("\\ma", list(maTier))
     return record
 
 recordOperations = {
-    "QToApostrophe": lambda record: Record([ [specialReplace(word, "q", "'") for word in tier ] for tier in record.tiers]),
+    #"QToApostrophe": lambda record: Record([ [specialReplace(word, "q", "'") for word in tier ] for tier in record.tiers], record.emptyLines),
     "ApostropheToQ": lambda record: createMa(record, lambda tier: (word.replace("'", "q") for word in tier)),
     "IvenaToIvaEna": lambda record: createMa(record, lambda tier: ("iva ena" if word=="ivena" else word for word in tier)),
-    "createMa": createMa
+    "IvonaToIvaOna": lambda record: createMa(record, lambda tier: ("iva ona" if word=="ivona" else word for word in tier)),
+    "IvamangToIvaAmang": lambda record: createMa(record, lambda tier: ("iva amang" if word=="ivamang" else word for word in tier)),
+    "IvamaToIvaAmang": lambda record: createMa(record, lambda tier: ("iva amang" if word=="ivama" else word for word in tier)),
+    "IvemiaToIvaEmia": lambda record: createMa(record, lambda tier: ("iva emia" if word=="ivemia" else word for word in tier)),
+    "IveriaToIvaEria": lambda record: createMa(record, lambda tier: ("iva eria" if word=="iveria" else word for word in tier)),
+    "IvavonaToIvaAbuOna": lambda record: createMa(record, lambda tier: ("iva abu ona" if word=="ivavona" else word for word in tier)),
+    "IvavenaToIvaAbuEna": lambda record: createMa(record, lambda tier: ("iva abu ena" if word=="ivavena" else word for word in tier)),
+    "IvavamangToIvaAbuAmang": lambda record: createMa(record, lambda tier: ("iva abu amang" if word=="ivavamang" else word for word in tier)),
+    "IvavamaToIvaAbuAmang": lambda record: createMa(record, lambda tier: ("iva abu amang" if word=="ivavama" else word for word in tier)),
+    "IvavemiaToIvaAbuEmia": lambda record: createMa(record, lambda tier: ("iva abu emia" if word=="ivavemia" else word for word in tier)),
+    "IvaveriaToIvaAbuEria": lambda record: createMa(record, lambda tier: ("iva abu eria" if word=="ivaveria" else word for word in tier)),
+
+    "AvonaToAbuOna": lambda record: createMa(record, lambda tier: ("abu ona" if word=="avona" else word for word in tier)),
+    "AvenaToAbuEna": lambda record: createMa(record, lambda tier: ("abu ena" if word=="avena" else word for word in tier)),
+    "AvamaToAbuAmang": lambda record: createMa(record, lambda tier: ("abu amang" if word=="avama" else word for word in tier)),
+    "AvamangToAbuAmang": lambda record: createMa(record, lambda tier: ("abu amang" if word=="avamang" else word for word in tier)),
+    "AvemiaToAbuEmia": lambda record: createMa(record, lambda tier: ("abu emia" if word=="avemia" else word for word in tier)),
+    "AveriaToAbuEria": lambda record: createMa(record, lambda tier: ("abu eria" if word=="averia" else word for word in tier)),
+
+    "XtoAsterisk": lambda record: createMa(record, lambda tier: ("*" if word=="XXX" else word for word in tier)),
+
+    "Veampeu": lambda record: createMa(record, lambda tier: ("veangpeu" if word=="veampeu" else word for word in tier)),
+    "VaToIva": lambda record: createMa(record, lambda tier: ("iva" if word=="va" else word for word in tier)),
+    "SenaToSoEna": lambda record: createMa(record, lambda tier: ("so ena" if word=="sena" else word for word in tier)),
+    "SonaToSoOna": lambda record: createMa(record, lambda tier: ("so ona" if word=="sona" else word for word in tier)),
+
+    "qataToXta": lambda record: createMa(record, lambda tier: ("xta" if re.fullmatch(r"[q'][aeiou]ta", word) else word for word in tier)),
+    "NgtaToXta": lambda record: createMa(record, lambda tier: ("xta" if word=="ngta" else word for word in tier)),
+
+    # Getting rid of single question marks surrounded by parentheses
+    "DeleteQM": lambda record: createMa(record, lambda tier: (word.replace(r"(?)", "") for word in tier)),
+
+    # Getting rid of all colons
+    "DeleteColon": lambda record: createMa(record, lambda tier: (word.replace(":", "") for word in tier)),
+
+    # Getting rid of single characters surrounded by parentheses
+    "PhonBrackets": lambda record: createMa(record, lambda tier: (re.sub(r"\((.+)\)", r"\1", word) for word in tier)),
+
+    "createMa": createMa,
+    "clean": Record.clean
 }
 
 if __name__ == '__main__':
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 import subprocess
+import os
 
 def callTest(fileNameInput, fileNameOutput = None, extraarg = []):
     if fileNameOutput == None:
@@ -22,6 +23,11 @@ def callTest(fileNameInput, fileNameOutput = None, extraarg = []):
 
 class TestIO(unittest.TestCase):
 
+    def _testDir(self, testDirectory, checks):
+        for inputFile in os.listdir("tests/" + testDirectory):
+            if inputFile.endswith(".text.in"):
+                self.assertEqual(callTest(testDirectory + '/' + inputFile, testDirectory + '/' + inputFile.replace(".in", ".out"), checks), 0)
+
     def test_invariant(self):
         self.assertEqual(callTest("a.text.in"), 0)
         self.assertEqual(callTest("b.text.in"), 0)
@@ -32,6 +38,7 @@ class TestIO(unittest.TestCase):
         self.assertEqual(callTest("testivena_mt.txt"), 0)
         self.assertEqual(callTest("testivena_ma.txt"), 0)
         self.assertEqual(callTest("bv_cs_nihenatuna_pt.txt"), 0)
+        self.assertEqual(callTest("deletion_tests/colon.text.out"), 0)
 
     def test_process(self):
         self.assertEqual(callTest("a.text.in", "a.text.out", ["ApostropheToQ"]), 0)
@@ -41,6 +48,22 @@ class TestIO(unittest.TestCase):
         self.assertEqual(callTest("ivaena.text.in", "ivaena.text.out", ["IvenaToIvaEna"]), 0)
 
         self.assertEqual(callTest("testivena_mt.txt", "testivena_ma.txt", ["IvenaToIvaEna"]), 0)
+
+    def test_deletion(self):
+        checks = ["DeleteColon", "DeleteQM", "PhonBrackets", "clean"]
+        self._testDir("deletion_tests", checks)
+
+    def test_replacements(self):
+        checks = ["XtoAsterisk", "SenaToSoEna", "SonaToSoOna", "VaToIva", "Veampeu"]
+        self._testDir("MoreReplacementTests", checks)
+
+    def test_xta(self):
+        checks = ["qataToXta", "NgtaToXta"]
+        self._testDir("xta_simple_tests", checks)
+
+    def test_apostrophe(self):
+        checks = ["qataToXta", "ApostropheToQ"]
+        self._testDir("xtaApostropheToQ", checks)
 
 if __name__ == '__main__':
     unittest.main()
