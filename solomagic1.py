d80779c02d67b60fa2b87f671517da0fb60569ed
Aleix Pol <aleixpol@kde.org>
2017-01-20 12:35:56 +0100

    Hits head against the wall
    Fixes #4

--- solomagic1.py
+++ solomagic1.py
@@ -4,6 +4,6 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input text file")
-    args = parser.parse_args(args.input)
+    args = parser.parse_args()
 
     process(parser.input, ["XtoAsterisk", "SenaToSoEna", "SonaToSoOna", "VaToIva", "Veampeu", "qataToXta", "NgtaToXta", "ApostropheToQ", "IvenaToIvaEna"])
