7e0243020afc9da7511c82adc1f6b0ab1171ef51
Aleix Pol <aleixpol@kde.org>
2017-01-21 02:38:26 +0100

    Fix solomagic1

--- solomagic1.py
+++ solomagic1.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     parser.add_argument("input", help="input text file")
     args = parser.parse_args()
 
-    process(parser.input, ["XtoAsterisk", "SenaToSoEna", "SonaToSoOna", "VaToIva", "Veampeu", "qataToXta", "NgtaToXta", "ApostropheToQ", "IvenaToIvaEna"])
+    solomagic.process(args.input, ["XtoAsterisk", "SenaToSoEna", "SonaToSoOna", "VaToIva", "Veampeu", "qataToXta", "NgtaToXta", "ApostropheToQ", "IvenaToIvaEna"])
